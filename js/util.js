f879ec347942dc84b1cbd30255ad6ab6eb9f6ff1
PElenaP <Elena.P.o.p.o.v.a@yandex.ru>
2022-12-13 07:46:45 +0500

    Отрисуй меня полностью часть 1

---
+++ js/util.js
@@ -0,0 +1,19 @@
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElementFromArray = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+const checkStringLength = (string, length) => string.length <= length;
+
+const uniqueNumberGenerator = () => {
+  let prevNumber = 0;
+
+  return () => ++prevNumber;
+};
+
+export {getRandomPositiveInteger, getRandomElementFromArray, checkStringLength, uniqueNumberGenerator};
