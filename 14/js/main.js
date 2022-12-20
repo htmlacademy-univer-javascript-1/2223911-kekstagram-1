6558fdbb49ff32d030d34abe948e69ebd796c2e7
Keks <keks@htmlacademy.ru>
2022-12-20 09:08:29 +0300

    :heavy_check_mark: Сборка #14

--- 14/js/main.js
+++ 14/js/main.js
@@ -1,7 +1,7 @@
 import {debounce} from './util.js';
 import {getData} from './api.js';
 import {createThumbnails} from './thumbnails.js';
-import {setUserFormSubmit, closeUploadFileForm} from './form.js';
+import {setUserFormSubmit, closeUploadFileForm} from './form-validation.js';
 import {thumbnailClickHandler} from './big-pictures.js';
 import {revealError, revealSuccess} from './notifications.js';
 import {setFilters, revealFilters, RERENDER_DELAY} from './filters.js';
